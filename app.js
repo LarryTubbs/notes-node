b7bbdac6428f5d2371bfe463e3628c75cd57a632
LarryTubbs <larry.tubbs@gmail.com>
2018-11-20 16:03:41 -0600

    used a few _ functions

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ console.log('Starting app.js');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes');
+const _ = require('lodash');
 
-console.log(notes.addNote());
-console.log(notes.add(1, 1));
+var filteredArray = _.uniq(['Larry', 1, 'Larry', 1, 2 , 3, 4]);
+console.log(filteredArray);
+console.log(_.isString(true));
+console.log(_.isString('Larry'));
+
+// console.log(notes.addNote());
+// console.log(notes.add(1, 1));
 
 // var user = os.userInfo();
 
